6fea13e0e15d75114e87d7c88ee3c96229fadd7f
jahanzaib-sajjad-brainx <jahanzaib.sajjad@brainxtech.com>
2022-06-17 20:49:15 +0500

    merged todo with user aunthetiacation

--- todo-list.js
+++ todo-list.js
@@ -1,12 +1,19 @@
 
 const express=require('express')
 const port=4000
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
 
 
-app.use("/", require('./routes/RouteClass'))
+app.use("/", require('./routes/TodoRoutes'))
+app.use("/",require('./routes/UsersRoutes'))
+
+mongoose.connect('mongodb://127.0.0.1/todolist')
+.then(res => {
+  console.log("Database connected successfully...")
+})
 
 
 app.listen(port, () => {
